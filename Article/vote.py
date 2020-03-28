02c527d59a49653d1c5a18f85ba040c43a20e4ba
liqinglin <531192557@qq.com>
2020-03-29 07:23:10 +0800

    文章分组

--- Article/vote.py
+++ Article/vote.py
@@ -39,4 +39,31 @@ def post_article(conn,user,title,link):
 # 获取文章
 ARTICLES_PER_PAGE=25
 
+def get_articles(conn,page,order='score:'):
+    start=(page-1)*ARTICLES_PER_PAGE
+    end =start+ARTICLES_PER_PAGE
 
+    ids=conn.zrevrange(order,start,end)
+    articles=[]
+    for id in ids:
+        article_data=conn.hgetall(id)
+        article_data['id']=id
+        articles.append(article_data)
+    return articles
+
+# 文章添加分组，移除分组
+def add_remove_groups(conn,article_id,to_add=[],to_remove=[]):
+    article='article:'+article_id
+    for group in to_add:
+        conn.sadd('group:'+group,article)
+    for group in to_remove:
+        conn.srem('group:'+group,article)
+
+# 将分组内的文章进行排序
+def get_group_articles(conn,group,page,order='score:'):
+    key=order+group
+    if not conn.exists(key):
+        # 如果不存在分组集合，和order有序集合的交集，那么进行排序
+        conn.zinterstore(key,['group:'+group,order],aggregate='max',)
+        conn.expire(key,60)
+    return get_articles(conn,page,key)
